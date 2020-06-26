a42058e44a9e0cb8c894ad17fc9b308927e239b8
Ian Hickson <ian@hixie.ch>
2020-06-25 18:31:19 -0700

    Simplify the widget structure and add a custom painter

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:forest_venture/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('World should cover screen', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(tester.getRect(find.byType(WorldCanvas)), Rect.fromLTWH(0.0, 0.0, 800.0, 600.0));
   });
 }
