4347f4ab07d42ebe08689c6877754a0333d0d6da
treeplate <tree.plate.moo@gmail.com>
2020-06-25 18:39:05 -0700

    Add world parser

--- lib/world.dart
+++ lib/world.dart
@@ -1,8 +1,26 @@
 class World {
-  World(this.width, this.height);
+  World(this.width, this.cells);
   final int width;
-  final int height;
-  Cell at(int x, int y) => Cell();
+  int get height => (cells.length / width).round();
+  final List<Cell> cells;
+  factory World.parse(List<String> data) {
+    List<Cell> parsed = [];
+    int width = 0;
+    for (String line in data) {
+      for (String char in line.split('')) {
+        switch (char) {
+          case " ":
+            parsed.add(Cell());
+            break;
+          default:
+            throw FormatException("Unexpected \"$char\" while parsing world");
+        }
+      }
+      width++;
+    }
+    return World(width, parsed);
+  }
+  Cell at(int x, int y) => cells[x + (y * width)];
 }
 
 class Cell {}
