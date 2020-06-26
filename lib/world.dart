2dab77d849cddd23eb9860d98bf165cf3cf3f6ad
treeplate <tree.plate.moo@gmail.com>
2020-06-26 14:15:23 -0700

    add player

--- lib/world.dart
+++ lib/world.dart
@@ -1,7 +1,9 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
+
 class World {
-  World(this.width, this.cells);
+  World(this.width, this.cells, this._playerx, this._playery);
   final int width;
   int get height {
     assert(cells.length.isFinite);
@@ -11,11 +13,39 @@ class World {
     return cells.length ~/ width;
   }
 
+  int _playerx;
+  int _playery;
+  int get playerx => _playerx;
+  int get playery => _playery;
+
+  void left() {
+    assert(playerx > 0);
+    if (at(playerx - 1, playery).clear) _playerx--;
+  }
+
+  void right() {
+    assert(playerx < width - 1);
+    if (at(playerx + 1, playery).clear) _playerx++;
+  }
+
+  void up() {
+    assert(playery > 0);
+    if (at(playerx, playery - 1).clear) _playery--;
+  }
+
+  void down() {
+    assert(playery < height - 1);
+    if (at(playerx, playery + 1).clear) _playerx++;
+  }
+
   final List<Cell> cells;
   factory World.parse(List<String> data) {
     List<Cell> parsed = [];
     int width = 0;
-    for (String line in data) {
+    List<String> xy = data.first.split(" ");
+    int x = int.parse(xy[0]);
+    int y = int.parse(xy[1]);
+    for (String line in data.toList()..removeRange(0, 1)) {
       cols:
       for (String char in line.split('')) {
         switch (char) {
@@ -30,7 +60,7 @@ class World {
       }
       width++;
     }
-    return World(width, parsed);
+    return World(width, parsed, x, y);
   }
   Cell at(int x, int y) => cells[x + (y * width)];
 }
@@ -39,4 +69,6 @@ World fromFile(String file) {
   return World.parse(File("lib/" + file).readAsLinesSync());
 }
 
-class Cell {}
+class Cell {
+  bool get clear => true;
+}
