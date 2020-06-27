628d7f3f86106220f88d46964158d163902be01c
treeplate <tree.plate.moo@gmail.com>
2020-06-27 10:11:50 -0700

    add movement test doesn't actually test anything, just tries to move the player

--- test/basic_world_test.dart
+++ test/basic_world_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:forest_venture/main.dart';
 import 'package:forest_venture/world.dart';
 
@@ -8,6 +7,17 @@ void main() {
     await tester.pumpWidget(
       WorldCanvas(world: World(1, <Cell>[Cell()], 0, 0)),
     );
-    await expectLater(find.byType(WorldCanvas), matchesGoldenFile('basic_world_1.png'));
+    await expectLater(
+        find.byType(WorldCanvas), matchesGoldenFile('basic_world_1.png'));
+  });
+  testWidgets('Movement', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      WorldCanvas(world: World(2, <Cell>[Cell(), Goal()], 0, 0)),
+    );
+    //TODO: Check image
+    await tester.tapAt(Offset(
+        tester.getCenter(find.byType(WorldCanvas)).dx + 60.0,
+        tester.getCenter(find.byType(WorldCanvas)).dy));
+    //TODO: Check image again
   });
 }
