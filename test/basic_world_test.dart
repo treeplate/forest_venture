d380e52752739072ebe9e281bd05f1ddfe971e59
treeplate <tree.plate.moo@gmail.com>
2020-06-27 16:14:46 -0700

    add move tests will need reupdating after #6 is fixed

--- test/basic_world_test.dart
+++ test/basic_world_test.dart
@@ -4,7 +4,6 @@ import 'package:forest_venture/main.dart';
 import 'package:forest_venture/world.dart';
 
 void main() {
-
   testWidgets('Most basic world', (WidgetTester tester) async {
     await tester.pumpWidget(
       GamePage(source: TestWorldSource(World(1, <Cell>[Empty()], 0, 0))),
@@ -18,10 +17,15 @@ void main() {
 
   testWidgets('Movement', (WidgetTester tester) async {
     await tester.pumpWidget(
-      GamePage(source: TestWorldSource(World(2, <Cell>[Empty(), Goal()], 0, 0))),
+      GamePage(
+          source: TestWorldSource(World(2, <Cell>[Empty(), Goal()], 0, 0))),
     );
     await tester.pump();
     //TODO: Check image
+    await expectLater(
+      find.byType(WorldCanvas),
+      matchesGoldenFile('basic_world_move_before.png'),
+    );
     await tester.tapAt(
       Offset(
         tester.getCenter(find.byType(WorldCanvas)).dx + 60.0,
@@ -29,8 +33,11 @@ void main() {
       ),
     );
     //TODO: Check image again
+    await expectLater(
+      find.byType(WorldCanvas),
+      matchesGoldenFile('basic_world_move_after.png'),
+    );
   });
-
 }
 
 @immutable
